44c5cb050f47053d8c64b5d90ad8cd428fb30da2
diglean <diegoleandro2002@gmail.com>
2022-06-01 17:11:52 -0300

    Using context

---
+++ src/components/Alunos/index.js
@@ -0,0 +1,18 @@
+//import Nome from '../Nome';
+import { useContext } from 'react';
+import Nome from '../Nome';
+
+import { UserContext } from '../../contexts/user';
+
+function Alunos(){
+    const { qtdAlunos } = useContext(UserContext);
+
+    return(
+        <div>
+            <h2>Quantidade total de alunos: {qtdAlunos}</h2>
+            <Nome/>
+        </div>
+    );
+}
+
+export default Alunos;
